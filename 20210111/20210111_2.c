d3741607a00c634f8fd1201bee05bc668ba8adad
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-12 16:51:41 +0200

    Add files via upload

---
+++ 20210111/20210111_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int vzemiBitNa(int iNum,int iPosition);
+
+int main(){
+   int iNum = 3;
+   printf("%d",vzemiBitNa(iNum,1));
+
+    return 0;
+
+    }
+
+    int vzemiBitNa(int iNum,int iPosition){
+        int iTemp = 1;
+        iNum = iNum >> iPosition;
+        iNum = iNum & iTemp; 
+
+        return iNum;
+    }
