8cc72e33dad0c3f708f8fbe2b18a1ef742aa5128
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-12 16:15:03 +0200

    Add files via upload

---
+++ 20210111/20210111_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a = 3 ;
+    int b = 5 ;
+
+    int andRez = a & b;
+    int orRez = a | b;
+    int xorRez = a ^ b;
+    
+    printf("%d\n", andRez);
+    printf("%d\n", orRez);
+    printf("%d\n", xorRez);
+
+    return 0;
+}
