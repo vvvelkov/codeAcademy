ecb81d2677ab26f80151b936856eda0d2fb54e41
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-12 18:56:15 +0200

    Add files via upload

---
+++ 20210111/20210111_10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+   int iTemp = 1,iTemp1 = 1,iNum = 15;//0000 1111
+   iTemp = iTemp << 2; //   0000 0100
+   iTemp1 = iTemp1 << 3; // 0000 1000
+   iTemp = iTemp + iTemp1;// 0000 1100
+   iTemp = ~iTemp; //      1111 0011
+   iNum = iNum & iTemp; // 0000 1111 = 0000 0011
+   printf("%d", iNum);
+
+    return 0;
+
+    }
