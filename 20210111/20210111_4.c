b7ae1c178803d0548b56a3e0144593d7ba31f2b7
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-12 17:48:46 +0200

    Add files via upload

---
+++ 20210111/20210111_4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int vdigniBitNa(int iNum,int iPos);
+
+int main(){
+   int iNum = 5;// a = 0000 0101
+   printf("%d", vdigniBitNa(iNum,1));
+
+    return 0;
+
+    }
+
+    int vdigniBitNa(int iNum,int iPos){
+        int iTemp = 1;
+        iTemp = iTemp << iPos;// 0000 0001
+        iNum =  iNum | iTemp;// 0000 0101 | 0000 0010 = 0000 0111
+
+        return iNum;
+    }
