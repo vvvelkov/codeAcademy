c05748f772591fd36c9c5de3a0f25c5ff82f892e
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-14 15:58:39 +0200

    Add files via upload

---
+++ 20210113/20210113_10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  char caravans = 3;
+  char carValue = 90; // lv
+  char campers = 3;
+  char camValue = 100; // lv
+  printf("Dobre doshli v Karavani i Kemperi pod naem\n");
+  printf("Ako zalaete karavana natisnete 1\n");
+  printf("Ako zalaete kemper natisnete 2\n");
+  int res;
+  scanf("%d",&res);
+
+    return 0;
+}
---
+++ 20210113/20210113_11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  double fTomatoPrice = 4.5; // za kg
+  printf("Hello, welcome to our shop!"); 
+  printf("Today we offer - fresh tomatoes flour,");
+  printf("yoghurt , ice cream and lollipops.\n");
+  printf("How much killos of tomatoes do you want?\n");
+  double fTomatoWeight;
+  scanf("%f",&fTomatoWeight);
+  double fTomatoTotal = fTomatoWeight*fTomatoPrice;
+    return 0;
+}
