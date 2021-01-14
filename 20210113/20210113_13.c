c46311d933f9a26038da8424f718f29eb5aad21f
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-14 16:19:39 +0200

    Add files via upload

---
+++ 20210113/20210113_13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  char a = 'a';
+  printf("%c", a);
+  printf("--> 1 byte\n");
+  int b = 102;
+  printf("%d", b);
+  printf("--> 4 bytes\n");
+  long c = 50000;
+  printf("%d", c);
+  printf("--> 4 bytes\n");
+  float d = 6.0;
+  printf("%.f",d);
+  printf("--> 4 bytes\n");
+  double e = -48.907;
+  printf("%.3f", e);
+  printf("--> 8 bytes\n");
+    return 0;
+}
