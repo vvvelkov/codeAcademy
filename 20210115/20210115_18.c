6e3ef449bc19cdde7c5c3a2bf12a6e4a86e50ab5
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-16 19:25:54 +0200

    Update 20210115_18.c

--- 20210115/20210115_18.c
+++ 20210115/20210115_18.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(){
-  char caravans = 3;
-  char carValue = 90; // lv
-  char campers = 3;
-  char camValue = 100; // lv
+  float caravans = 3;
+  float carValue = 90; // lv
+  float campers = 3;
+  float camValue = 100; // lv
   printf("Dobre doshli v Karavani i Kemperi pod naem\n");
   printf("Ako zalaete karavana natisnete 1\n");
   printf("Ako zalaete kemper natisnete 2\n");
   int res;
   scanf("%d",&res); // choose 1
-  char moneyCar = carValue / caravans;
+  float moneyCar = carValue / caravans;
   printf("Duljite ni %d lv\n",moneyCar);
   printf("Jelaete li oshte nalichni produkti?"); 
-
-
     return 0;
 }
