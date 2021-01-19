c68bd172f2d7c3c2d82a1c13758c0fd689e2e436
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-19 21:37:03 +0200

    Update 20210113_10.c

--- 20210113/20210113_10.c
+++ 20210113/20210113_10.c
@@ -2,14 +2,15 @@
 
 int main(void){
   char caravans = 3;
-  char carValue = 90; // lv
+  unsigned char carValue = 90; // lv
   char campers = 3;
-  char camValue = 100; // lv
+  unsigned char camValue = 100; // lv
   printf("Dobre doshli v Karavani i Kemperi pod naem\n");
   printf("Ako zalaete karavana natisnete 1\n");
   printf("Ako zalaete kemper natisnete 2\n");
   int res;
   scanf("%d",&res);
+  printf("You chose %d", res);
 
     return 0;
 }
