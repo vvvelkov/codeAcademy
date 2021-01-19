752174c00518ed0a5d4d922f5b5d5b4ea76beb93
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-19 22:17:15 +0200

    Update 20210115_7.c

--- 20210115/20210115_7.c
+++ 20210115/20210115_7.c
@@ -4,6 +4,6 @@ int main(void){
   double f = 0xBB;
   signed long long i = 0xBB;
   unsigned long long a = 0xBBu;
-  printf("f = %lf , i = %lld , a = %lld", f , i , a);
+  printf("f = %lf , i = %lld , a = %llu", f , i , a);
 return 0;
 }
