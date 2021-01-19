f61411d2844e7247a8bbeff606dcb7fe3c89ea90
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-19 21:48:49 +0200

    Update 20210113_11.c

--- 20210113/20210113_11.c
+++ 20210113/20210113_11.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int main(void){
-  double fTomatoPrice = 4.5; // za kg
+  float tomatoPrice = 4.5;
+  float flourPrice = 1.80;
+  float yogourtPrice = 0.50;
+  float icecreamPrice = 0.60;
+  float candyPrice = 1.50;
+  float lolipopPrice = 0.15;
+  float tomatoWeight, flourWeight, candyWeight;
+  short int yogourtNumber, icecreamNumber, lolipopNumber;
+ 
   printf("Hello, welcome to our shop!"); 
   printf("Today we offer - fresh tomatoes flour,");
   printf("yoghurt , ice cream and lollipops.\n");
   printf("How much killos of tomatoes do you want?\n");
-  double fTomatoWeight;
-  scanf("%f",&fTomatoWeight);
-  double fTomatoTotal = fTomatoWeight*fTomatoPrice;
+ 
+  scanf("%f",&tomatoWeight);
+  float tomatoTotal = tomatoWeight*tomatoPrice;
+  
+  printf("You wanted quantity which is equal to %.2f lv\n",tomatoTotal);
+  printf("How many lolipops do you want?\n");
+
+  scanf("%d", &lolipopNumber);
+  float lolipopTotal = lolipopNumber*lolipopPrice;
+
+  printf("You wanted quantity which is equal to %.2f lv\n", lolipopTotal);
+
     return 0;
 }
