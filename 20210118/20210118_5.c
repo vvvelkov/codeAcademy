e27a8c3e4e632c3379b1bd3204134c686fc622c8
Valentin Velkov <76812375+vvvelkov@users.noreply.github.com>
2021-01-19 16:02:37 +0200

    Create 20210118_5.c

---
+++ 20210118/20210118_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 1, y = 0;
+    int z = y || x;
+    printf("%d\n", z);
+
+    printf("\nInit values: X= %d, Y= %d\n", x, y);
+    x = 1 + 2, 2 * 3, 3 + 4;
+    y = (7 * 8, 8 + 9, 9 - 4);
+    printf("\nX= %d, Y= %d\n", x, y); 
+    return 0;
+}
